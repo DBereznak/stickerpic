7564e0b4a43f66b3c85fe8c3f66e59efa07190b1
Donny Bereznak <dbereznak@gmail.com>
2024-10-09 14:47:07 -0400

    added save button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import BackgroundSlider from './components/BackgroundSlider/BackgroundSlider';
 import StickerScene from './components/StickerScene/StickerScene';
 import './App.css';
 
 
-function App() {
+function App() { 
+  function downloadImage() {
+    const canvas = document.getElementById("scene");
+    const scene = canvas.toDataURL("image/png");
+    const fakeLink = document.createElement("a");
+    fakeLink.href = scene;
+    fakeLink.download = 'scene.png';
+    fakeLink.click();
+  }
 
   return (
     <>
       <BackgroundSlider />
       <StickerScene />
+      <button onClick={downloadImage}>Download Scene</button>
     </>
   )
 };
--- src/components/StickerScene/StickerScene.jsx
+++ src/components/StickerScene/StickerScene.jsx
@@ -1,15 +1,24 @@
 import { useSelector } from "react-redux";
+import { useRef, useEffect} from 'react';
 import './StickerScene.css';
 
 function StickerScene() {
     const background = useSelector((state) => state.scene.background);
-    const backgroundStyle = {
-        background: `no-repeat url(${background})`,
-    };
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+        const bg = new Image(1280, 640);
+        bg.src = background;
+        context.font = "50px Arial";
+        context.drawImage(bg, 0, 0);
+        context.fillText("Scene",10,80);
+    }, [background])
     return (
-        <div style={backgroundStyle} className="background"> 
-        </div>
+        <canvas id="scene" width="1280" height="640" ref={canvasRef}> 
+        </canvas>
     )
 };
 
-export default StickerScene;
+export default StickerScene;        
